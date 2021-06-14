640ddea39642421fa9cf13682066a997bfa59d3b
Harrison A. Pickett, IV <38798125+harrisonapickettiv@users.noreply.github.com>
2021-06-14 16:08:15 -0500

    Parse dice notaion in the form 'NdX' return a result

--- test/diceRoller.test.js
+++ test/diceRoller.test.js
@@ -44,3 +44,25 @@ test('roll("1d4") returns an object where the property "total" is a number betwe
     expect(roll.total).toBeLessThanOrEqual(4);
   });
 });
+
+describe('Tests with roll("2d4")', () => {
+  const diceRolls = [];
+  beforeAll(() => {
+    for (let i = 0; i < testCount; i++) {
+      diceRolls.push(roll('2d4'));
+    }
+  });
+
+  test('roll("2d4") returns an object where the property "total" is a number between 2 and 8', () => {
+    diceRolls.forEach((roll) => {
+      expect(roll.total).toBeGreaterThanOrEqual(2);
+      expect(roll.total).toBeLessThanOrEqual(8);
+    });
+  });
+
+  test('roll("2d4") returns an object where "total" is equal to the sum of "results"', () => {
+    diceRolls.forEach(({ total, results }) => {
+      expect(results.reduce((acc, i) => acc + i, 0)).toBe(total);
+    });
+  });
+});
