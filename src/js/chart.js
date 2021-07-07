4c548d3d1bd90e46dff716facb286d04a043a021
Harrison A. Pickett, IV <38798125+harrisonapickettiv@users.noreply.github.com>
2021-07-07 11:10:00 -0500

    Allow user to remove a specific dataset

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,9 +1,10 @@
-import { updateChartData } from './histogram';
+import { updateChartData, removeDataset } from './histogram';
 
-const { Chart, randomColor } = window;
+const { Chart, cuid, randomColor } = window;
 Chart.defaults.color = '#000000';
 Chart.defaults.borderColor = '#000000';
 
+const chartData = document.getElementById('chartData');
 const ctx = document.getElementById('diceRollChart').getContext('2d');
 const diceChart = new Chart(ctx, {
   type: 'bar',
@@ -24,20 +25,42 @@ const selectColor = () => {
   return color;
 };
 
+const createDataRemoveBtn = (datasetID, color, rollExpr) => {
+  const div = document.createElement('div');
+  const a = document.createElement('a');
+  div.setAttribute('id', datasetID);
+  div.setAttribute('class', 'col p-1 m-1 rounded-left rounded-right');
+  div.setAttribute('style', `background-color: ${color};`);
+  a.setAttribute('class', 'btn btn-sm');
+  a.innerHTML = `&times;&nbsp;${rollExpr}`;
+  a.onclick = (event) => {
+    event.target.parentNode.remove();
+    diceChart.data = removeDataset(diceChart.data, event.target.parentNode.id);
+    diceChart.update();
+  };
+  div.append(a);
+  chartData.append(div);
+};
+
 const updateChart = () => {
   const trials = parseInt(document.getElementById('trials').value);
   const rollExpr = document.getElementById('rollExpression').value || '4d6h3';
+  const datasetID = cuid();
+  const color = selectColor();
 
+  createDataRemoveBtn(datasetID, color, rollExpr);
   diceChart.data = updateChartData(
     rollExpr,
     trials,
-    selectColor(),
-    diceChart.data
+    color,
+    diceChart.data,
+    datasetID
   );
   diceChart.update();
 };
 
 const resetChart = () => {
+  chartData.innerHTML = null;
   diceChart.data = {};
   diceChart.update();
 };
